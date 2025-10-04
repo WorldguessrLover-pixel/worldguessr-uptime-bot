1634056c0cde04307d5dfcf02c2e79b2079ad414
WorldguessrLover-pixel <WorldguessrLover@outlook.fr>
2025-10-04 09:24:54 +0200

    Update storage.py

--- storage.py
+++ storage.py
@@ -11,14 +11,14 @@ def load_data():
             with open(STORAGE_FILE, "r", encoding="utf-8") as f:
                 return json.load(f)
         except json.JSONDecodeError:
-            return {"last_check": None, "players": []}
-    return {"last_check": None, "players": []}
+            return {"last_check": None, "users": []}
+    return {"last_check": None, "users": []}
 
-def save_data(players):
-    """Sauvegarde les joueurs avec un timestamp dans storage.json"""
+def save_data(users):
+    """Sauvegarde les joueurs avec leur elo + timestamp"""
     data = {
         "last_check": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-        "players": players
+        "users": users
     }
     with open(STORAGE_FILE, "w", encoding="utf-8") as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
